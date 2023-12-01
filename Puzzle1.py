17d2c5f143d653aded6a70f1fcf14a7845492738
Vierce1 <a.westermann.19@gmail.com>
2023-12-01 14:43:48 -0600

    forgot zero is a digit

--- Puzzle1.py
+++ Puzzle1.py
@@ -2,7 +2,7 @@ import file_ops
 
 
 part_two = True
-num_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5,
+num_dict = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5,
             'six':6, 'seven':7, 'eight':8, 'nine':9}
 
 input_lines = file_ops.read_input(1)
