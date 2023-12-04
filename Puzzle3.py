675e7166377740320ea46453640c5692a83779f6
Vierce1 <a.westermann.19@gmail.com>
2023-12-04 13:44:18 -0600

    3 both parts solved

--- Puzzle3.py
+++ Puzzle3.py
@@ -1,49 +1,83 @@
 import file_ops
 
 
-# class SchematicItem:
-#     def __init__(self, row: int, columns: list[int]):
-#         self.row = row
-#         self.columns = columns
-#
-# class PartNumber(SchematicItem):
-#     def __init__(self, row: int, columns: list[int], part_number: int):
-#         super(PartNumber, self).__init__(row, columns)
-#         self.part_number = part_number
+def get_number(row: str, char_index: int) -> str:  # part 1 method
+    part_number = ''
+    while char_index < len(row) and row[char_index].isdecimal():
+        part_number += line[char_index]
+        char_index += 1
+    return part_number
+
 
 input_lines = file_ops.read_input(3)
 part_number_sum = 0
+gear_ratio = 0
 
-# row,index set containing symbol positions
-symbol_positions: {(int, int)} = {}
 for r, line in enumerate(input_lines):
     c = 0
-    for _ in range(len(line)):
+    while c < len(line):
         char = line[c]
-        number = char
-        char_skip = 1  # to know length of part number digits
-        if char.isdecimal():
-            char_index = c + 1
-            while char_index < len(line) and line[char_index].isdecimal():
-                number += line[char_index]
-                char_index += 1
-            # Completed part number. Check for surrounding symbols
+        char_skip = 1  # to know length of part number digits for part 1
+
+        # ____PART 2______
+        if char == '*':
+            gear_nums = []
             for i in range(r - 1, r + 2):
                 if i < 0 or i >= len(input_lines):
                     continue  # outside bounds of input
-                for j in range(c - 1, c + len(number) + 2):
-                    if (i == r and (j != c - 1 or j != c + 1)) or (j < 0 or j >= len(line)):
+                j = (c - 1)
+                following_char_count = 1
+                while j < c + 2:
+                    if j < 0 or j >= len(line):
+                        continue  # out of bounds
+                    item = input_lines[i][j]
+                    gear_num = item
+                    if item.isdecimal():
+                        # check if end of number
+                        prev_char = j - 1
+                        # print(f' found {gear_num}')
+                        while prev_char >= 0 and input_lines[i][prev_char].isdecimal():
+                            gear_num = input_lines[i][prev_char] + gear_num
+                            prev_char -= 1
+                        # check if start of number
+                        next_char = j + 1
+                        while next_char < len(line) and input_lines[i][next_char].isdecimal():
+                            gear_num = gear_num + input_lines[i][next_char]
+                            next_char += 1
+                            following_char_count += 1
+                        gear_nums.append(gear_num)
+
+                    if len(gear_nums) > 2:
+                        break
+                    j += following_char_count
+                if len(gear_nums) > 2:
+                    break
+            if len(gear_nums) == 2:
+                # valid gear. multiply them together
+                gear_ratio += (int(gear_nums[0]) * int(gear_nums[1]))
+
+        # ____PART 1______
+        elif char.isdecimal():
+            finished_analyzing = False
+            number = get_number(line, c)
+            # Completed part number. Check for surrounding symbols
+            char_skip = len(number)
+            for i in range(r - 1, r + 2):
+                if i < 0 or i >= len(input_lines):
+                    continue  # outside bounds of input
+                for j in range(c - 1, c + len(number) + 1):
+                    if (i == r and (j != c - 1 and j != c + len(number))) or (j < 0 or j >= len(line)):
                         continue  # on the part number or out of bounds of line
                     item = input_lines[i][j]
                     if item != '.' and not item.isdecimal():  # safety
-                        print(number)
                         part_number_sum += int(number)
-                        char_skip = len(number)
+                        finished_analyzing = True
                         break
-                if char_skip > 1:
+                if finished_analyzing:
                     break
         c += char_skip
         if c >= len(line):
             break
 
-print(part_number_sum)
+print(f' Part 1 : {part_number_sum}')
+print(f' Part 2 : {gear_ratio}')
