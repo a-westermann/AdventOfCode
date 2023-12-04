d89883db09a312620132c43c485bf37d1e054b57
Andrew <a.westermann.19@gmail.com>
2023-12-03 18:07:21 -0600

    bones of puzzle 3

--- Puzzle3.py
+++ Puzzle3.py
@@ -1,6 +1,16 @@
 import file_ops
 
 
+class SchematicItem:
+    def __init__(self, row: int, columns: list[int]):
+        self.row = row
+        self.columns = columns
+
+class PartNumber(SchematicItem):
+    def __init__(self, row: int, columns: list[int], part_number: int):
+        super(PartNumber, self).__init__(row, columns)
+        self.part_number = part_number
+
 input_lines = file_ops.read_input(3)
 # Compare the indices of numbers & symbols which are 0-1 lines apart
 # for multi-digit numbers, include all corresponding indices
@@ -12,4 +22,7 @@ input_lines = file_ops.read_input(3)
 # Collect those indices
 # To find symbols, if parsing as int fails, check if period
 
+# row,index set containing symbol positions
+symbol_positions: {(int, int)} = {}
 for line in input_lines:
+    for
