bd3be0523bb0eaf41d7b1804fe79ad7a1ea051c3
Vierce1 <a.westermann.19@gmail.com>
2023-12-17 17:09:51 -0600

    17 setup, path director

--- path_directing.py
+++ path_directing.py
@@ -1,3 +1,4 @@
+import math
 from itertools import permutations
 
 
@@ -12,12 +13,16 @@ class Node:
         self.x = x
         self.y = y
         self.val = val
-        self.direction = None
+        self.val_to_target = math.inf
+        self.direction = -1
+        self.straight_count = 1
+        self.parent_node = None  # the node that this one leads to. Avoid backtracking
 
 
 class PathDirector:
     def __init__(self, input_lines: list[str]):
         self.grid: list[list[Node]] = []
+        self.queue = []
         for x in range(len(input_lines[0])):
             self.grid.append([])
             for y in range(len(input_lines)):
@@ -26,17 +31,38 @@ class PathDirector:
     def nodes(self, x, y) -> Node:
         return self.grid[x][y]
 
-    def get_neighbors(self, x, y) -> list[Node]:
+    def get_neighbors(self, xy) -> list[tuple[Node, int]]:
         # neighbors = permutations(range(-1, 2), 2)  # Diagonals
-        neighbor_positions = [(x + x2, y + y2) for (x2, y2) in [(0, -1), (0, 1), (-1, 0), (1, 0)]
-                              if 0 <= x + x2 < len(self.grid) and 0 <= y + y2 < len(self.grid[0])]
+        neighbor_positions = [((xy[0] + x2, xy[1] + y2), i) for i, (x2, y2) in
+                              enumerate([(-1, 0), (1, 0), (0, -1), (0, 1)])
+                              # enumerate([(0, -1), (0, 1), (-1, 0), (1, 0)])
+                              if 0 <= xy[0] + x2 < len(self.grid) and 0 <= xy[1] + y2 < len(self.grid[0])]
         # print(f'neighbor pos: {neighbor_positions}')
-        return [self.nodes(x2, y2) for x2, y2 in neighbor_positions]
+        return [(self.nodes(x2, y2), i) for (x2, y2), i in neighbor_positions]
 
     def direct_pathing(self, target_position: (int, int), dir_sync_max: int):
         target = self.grid[target_position[0]][target_position[1]]
+        if target.direction == -1:
+            target.val_to_target = target.val  # final target
+            target.straight_count = 1
         # radiate to nodes surrounding, pass in sum of total value to reach the target
         # overwrite if new value is lower than that node's current value
         # (& dir_sync_max is adhered to)
-
-
+        for n, i in self.get_neighbors(target_position):
+            if n == target.parent_node or \
+                    (i == target.direction and  # -1 to handle the final target
+                    target.straight_count == dir_sync_max):
+                continue  # hit the max straight concurrent directions
+            if target.val_to_target + n.val < n.val_to_target:
+                # found a lower value target. Update the path for this node & propogate
+                n.val_to_target = n.val + target.val_to_target
+                n.direction = i
+                n.parent_node = target
+                if i == target.direction or target.direction == -1:  # update the straight count
+                    n.straight_count = target.straight_count + 1
+                # if n not in self.queue:
+                self.queue.append(n)
+        if len(self.queue) == 0:
+            return self.grid
+        next_node = self.queue.pop(0)
+        return self.direct_pathing((next_node.x, next_node.y), dir_sync_max)
--- puzzle17.py
+++ puzzle17.py
@@ -1,6 +1,7 @@
 import file_ops
 from timer import TimeHandler
 from path_directing import PathDirector, Node
+import sys
 
 
 timer = TimeHandler()
@@ -10,6 +11,12 @@ for r in path_director.grid:
     print(''.join(str(node.val) for node in r))
 
 # part 1
-r = path_director.direct_pathing((len(path_director.grid) - 1, len(path_director.grid[0]) - 1), 3)
-
+sys.setrecursionlimit(999999)
+# val_grid = path_director.direct_pathing((len(path_director.grid[0]) - 1, len(path_director.grid) - 1), 3)
+val_grid = path_director.direct_pathing((0, 0), 3)
+for row in val_grid:
+    print([r.val_to_target for r in row])
+for row in val_grid:
+    print([r.direction for r in row])
+print(f'Part 1: {path_director.nodes(len(path_director.grid[0]) - 1, len(path_director.grid) - 1).val_to_target}')
 print(timer.fetch_time())
